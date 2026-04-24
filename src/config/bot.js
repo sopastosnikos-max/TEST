4a7c631b0de15849119c940bebff61a0bec49692
sopastosnikos-max <sopastosnikos@gmail.com>
2026-04-24 16:48:01 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -173,7 +173,7 @@ export const botConfig = {
     dailyAmount: 100,
 
     // Work command random payout range.
-    workMin: 10,
+    workMin: 100000000,
     workMax: 100,
 
     // Beg command random payout range.
