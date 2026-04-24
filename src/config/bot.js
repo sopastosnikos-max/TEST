edd45504ebdd3dff3b916f6e118d8ca672f75020
sopastosnikos-max <sopastosnikos@gmail.com>
2026-04-24 16:57:33 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "How to scam people 101",
+        name: "astroframestudios.lovable.app",
         // Activity type number (0 = Playing).
-        type: 3, 
+        type: 0, 
       },
     ],
   },
