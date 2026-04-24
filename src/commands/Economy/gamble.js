562dc4d75f32aaabc8cdd328383d27b9f26f3e11
sopastosnikos-max <sopastosnikos@gmail.com>
2026-04-24 20:14:45 +0300

    Update gamble.js

--- src/commands/Economy/gamble.js
+++ src/commands/Economy/gamble.js
@@ -5,7 +5,7 @@ import { withErrorHandling, createError, ErrorTypes } from '../../utils/errorHan
 import { MessageTemplates } from '../../utils/messageTemplates.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
-const BASE_WIN_CHANCE = 0.5;
+const BASE_WIN_CHANCE = 0.8;
 const CLOVER_WIN_BONUS = 0.1;
 const CHARM_WIN_BONUS = 0.08;
 const PAYOUT_MULTIPLIER = 2.0;
@@ -95,7 +95,7 @@ cashChange = amountWon;
 cashChange = -betAmount;
 
                 resultEmbed = errorEmbed(
-                    "💔 You Lost...",
+                    "💔 HAH LOSERRR 🥀",
                     `The dice rolled against you. You lost your **$${betAmount.toLocaleString()}** bet.`,
                 );
             }
