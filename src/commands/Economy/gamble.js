9851ab77bb8ad6306a7551d51b742849271b049f
sopastosnikos-max <sopastosnikos@gmail.com>
2026-04-25 14:26:58 +0300

    Update gamble.js

--- src/commands/Economy/gamble.js
+++ src/commands/Economy/gamble.js
@@ -5,9 +5,9 @@ import { withErrorHandling, createError, ErrorTypes } from '../../utils/errorHan
 import { MessageTemplates } from '../../utils/messageTemplates.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
-const BASE_WIN_CHANCE = 0.5;
-const CLOVER_WIN_BONUS = 0.5;
-const CHARM_WIN_BONUS = 0.08;
+const BASE_WIN_CHANCE = 0.10;
+const CLOVER_WIN_BONUS = 0.10;
+const CHARM_WIN_BONUS = 0.10;
 const PAYOUT_MULTIPLIER = 2.0;
 const GAMBLE_COOLDOWN = 0*60*1000;
 
